6b942ca60198261f7c260de53daf1a051f1562ac
Mulkhi Putra <putra@192.168.100.4>
2025-10-07 15:01:33 +0700

    add folder routes

---
+++ project/routes/Mahasiswa.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const MahasiswaRouter = express.Router();
+const app = express();
+const db = require("../connection");
+const response = require("../response");
+
+
+MahasiswaRouter
+  .route("/")
+
+  // GET ALL
+  .get((req, res) => {
+    const sql = "SELECT * FROM mahasiswa";
+    db.query(sql, (err, results) => {
+      if (err) return response(500, err, "Error fetching data", res);
+      return response(200, results, "List Mahasiswa", res);
+    });
+  })
+
+  // CREATE
+  .post((req, res) => {
+    const { nim, nama_lengkap, kelas, alamat } = req.body;
+    if (!nim || !nama_lengkap || !kelas || !alamat) {
+      return response(400, null, "All fields are required", res);
+    }
+
+    const sql =
+      "INSERT INTO mahasiswa (nim, nama_lengkap, kelas, alamat) VALUES (?, ?, ?, ?)";
+    db.query(sql, [nim, nama_lengkap, kelas, alamat], (err, results) => {
+      if (err) return response(500, err, "Error inserting data", res);
+      if (results.affectedRows > 0) {
+        return response(
+          201,
+          { isSuccess: true, id: nim },
+          "Mahasiswa created",
+          res
+        );
+      }
+      return response(500, "Insertion failed", "Error inserting data", res);
+    });
+  })
+
+  // UPDATE
+  .put((req, res) => {
+    const { nim, nama_lengkap, kelas, alamat } = req.body;
+    if (!nim) return response(400, null, "NIM is required", res);
+
+    const sql = `UPDATE mahasiswa SET nama_lengkap = ?, kelas = ?, alamat = ? WHERE nim = ?`;
+    db.query(sql, [nama_lengkap, kelas, alamat, nim], (err, results) => {
+      if (err) return response(500, err, "Error updating data", res);
+      if (results.affectedRows > 0) {
+        return response(
+          200,
+          { isSuccess: true, id: nim },
+          "Mahasiswa updated",
+          res
+        );
+      }
+      return response(404, "NIM not found", "Error updating data", res);
+    });
+  })
+
+  // DELETE
+  .delete((req, res) => {
+    const { nim } = req.body;
+    if (!nim) return response(400, null, "NIM is required", res);
+
+    const sql = `DELETE FROM mahasiswa WHERE nim = ?`;
+    db.query(sql, [nim], (err, results) => {
+      if (err) return response(500, err, "Error deleting data", res);
+      if (results.affectedRows > 0) {
+        return response(
+          200,
+          { isSuccess: true, id: nim },
+          "Mahasiswa deleted",
+          res
+        );
+      }
+      return response(404, "NIM not found", "Error deleting data", res);
+    });
+  });
+
+  module.exports = MahasiswaRouter;
